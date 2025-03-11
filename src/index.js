f2cf3c98a53e6dd5f4bf89752848b27c8fa0ca70
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2025-03-11 23:59:02 +0530

    parameter check

--- src/index.js
+++ src/index.js
@@ -5,34 +5,42 @@
  * @returns {number} Distance in kilometers
  */
 function calculateDistance(point1, point2) {
-    // console.log(point1, point2);
-    // Earth's radius in kilometers
-    const RADIUS = 6371;
 
-    // Convert latitude and longitude from degrees to radians
-    const latitude1Radians = (point1.lat * Math.PI) / 180;
-    const latitude2Radians = (point2.lat * Math.PI) / 180;
-    const differenceInLatitude = latitude2Radians - latitude1Radians;
+  // Ensure we have valid coordinates otherwise return error message and 0
+  if (
+    typeof point1.lat !== "number" ||
+    typeof point1.lng  !== "number" ||
+    typeof point2.lat !== "number" ||
+    typeof point2.lng !== "number"
+  ) {
+    return 0; 
+  }
 
-    const longitude1Radians = (point1.lng * Math.PI) / 180;
-    const longitude2Radians = (point2.lng * Math.PI) / 180;
-    const differenceInLongitude = longitude2Radians - longitude1Radians;
+  // Earth's radius in kilometers
+  const RADIUS = 6371;
 
-    // Haversine formula
-    const a =
-        Math.sin(differenceInLatitude / 2) * Math.sin(differenceInLatitude / 2) +
-        Math.cos(latitude1Radians) *
-        Math.cos(latitude2Radians) *
-        Math.sin(differenceInLongitude / 2) *
-        Math.sin(differenceInLongitude / 2);
+  // Convert latitude and longitude from degrees to radians
+  const latitude1Radians = (point1.lat * Math.PI) / 180;
+  const latitude2Radians = (point2.lat * Math.PI) / 180;
+  const differenceInLatitude = latitude2Radians - latitude1Radians;
 
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    // Distance in kilometers
-    const distance = RADIUS * c;
-    // console.log(distance);
-    return distance;
+  const longitude1Radians = (point1.lng * Math.PI) / 180;
+  const longitude2Radians = (point2.lng * Math.PI) / 180;
+  const differenceInLongitude = longitude2Radians - longitude1Radians;
+
+  // Haversine formula
+  const a =
+    Math.sin(differenceInLatitude / 2) * Math.sin(differenceInLatitude / 2) +
+    Math.cos(latitude1Radians) *
+      Math.cos(latitude2Radians) *
+      Math.sin(differenceInLongitude / 2) *
+      Math.sin(differenceInLongitude / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  // Distance in kilometers
+  const distance = RADIUS * c;
+  return distance;
 }
-
 
 /**
  * Recommend events for a user
